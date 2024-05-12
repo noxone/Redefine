5be9c4cf4a1450c91b8dd59aee7f9715ba97a9ba
Olaf Neumann <github@olafneumann.org>
2024-05-12 15:00:14 +0200

    Set up Spotify SDK

---
+++ Redefine/Redefine-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Redefine-Bridging-Header.h
+//  Redefine
+//
+//  Created by Olaf Neumann on 12.05.24.
+//
+
+#ifndef Redefine_Bridging_Header_h
+#define Redefine_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* Redefine_Bridging_Header_h */
